e373e10ef38630d2ff1d63dc0bf31e5f8989db5c
b-kuhu <Kuhu Bhatnagar>
2022-08-29 13:53:04 +0530

    Initial commit

---
+++ src/routes/getAllUsers.ts
@@ -0,0 +1,16 @@
+import {Request,Response} from 'express';
+export default function getAllUsers(req:Request,res:Response){
+    const users=[
+
+        {
+            name:'John',
+            age:30
+        },
+        {
+            name:'Peter',
+            age:40
+        }
+    ];
+ res.statusCode=200;
+ res.send({users});
+}
